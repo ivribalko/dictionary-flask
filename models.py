3d720a8ca624dad39ed61e66c094e9779c81547b
Ivan Rybalko <ivribalko@gmail.com>
2017-12-26 22:56:48 +0300

    misc

--- models.py
+++ models.py
@@ -2,6 +2,6 @@ from main import db
 
 class Vocabulary(db.Model):
     __tablename__ = 'vocabulary'
-
     word = db.Column(db.String(), primary_key=True)
     synonym = db.Column(db.String())
+
--- route.py
+++ route.py
@@ -5,17 +5,36 @@ from models import Vocabulary
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        word = request.form['word']
-        synonym = request.form['synonym']
-        record = Vocabulary(word=word, synonym=synonym)
-        db.session.add(record)
-        db.session.commit()
-        flash('Added')
-        return redirect(url_for('index')) #TODO 
-    return render_template('addword.html')
+    if request.method == 'GET':
+        return render_template('add.html')
+        
+    word = request.form['word']
+    synonym = request.form['synonym']
+
+    if not word or not synonym:
+        flash('Пусто')
+    elif word_exists(word):
+        flash('Уже есть')
+    else:
+        word_add(word, synonym)
+        flash('Добавил')
+    return redirect(url_for('index')) #TODO 
 
 @app.route('/list/')
 def show_word_list():
-    words = Vocabulary.query.all()
+    words = word_list()
     return render_template('list.html', words=words)
+
+def word_exists(word):
+    query = Vocabulary.query.filter_by(word=word)
+    result = db.session.query(query.exists())
+    return result.scalar()
+
+def word_add(word, synonym):
+    record = Vocabulary(word=word, synonym=synonym)
+    db.session.add(record)
+    db.session.commit()
+    return True
+
+def word_list():
+    return Vocabulary.query.all()
